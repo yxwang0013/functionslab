740695457be4a84872416d6adb60e8a8db6a6f94
Yuxuan Wang <2879296442@qq.com>
2020-10-04 17:58:05 +0100

    Exercise 1

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+     const completed = json.filter((todo) => todo.completed
+     )
+     completed.forEach( (todo, index) => {
+      console.log(`${todo.title} - ${todo.completed}`)
+    })
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
